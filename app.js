218ba7f74b7f60ae0e10a2d8588bf9b309f69889
plantleafboy <alexhpcp@gmail.com>
2023-07-14 02:25:33 +1200

    add code to pick winner of game. Require fix on winner scope for function access

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
+const outcomes = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+};
+
 function getComputerChoice() {
-    let choice;
-
-
-    return choice;
+    let choices = ["paper", "scissors", "rock"];
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(choices[randomNumber])
+    return choices[randomNumber];
+    
 }
 
 function playRound(playerSelection, computerSelection) {
-    p_selection
-    c_selection 
-    if (playerSelection === "rock") { //logic for picking winner
-        if (computerSelection === "scissors") {
-
-        }
+    if (playerSelection === computerSelection) { //logic for picking winner
+        return `Tie! you both picked ${playerSelection}!`; //if tie
     }
-    return `You Win! ${playerSelection} beats ${computerSelection}!`; //if player win
-    return `You Lose! ${computerSelection} beats ${playerSelection}!`; //if player lose
-    return `Tie! you both picked ${playerSelection}!`; //if tie
+    else if (outcomes[playerSelection] === computerSelection) {
+        playerWins++;
+        return `You Win! ${playerSelection} beats ${computerSelection}!`; //if player win
+    }
+    else {
+        computerWins++;
+        return `You Lose! ${computerSelection} beats ${playerSelection}!`; //if player lose
+    }
 }
 
-const playerSelection = prompt("paper scissors or rock?");
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+
+    if (playerWins !== 5 || computerWins !== 5) {
+        let playerSelection = prompt("paper, scissors or rock?");
+        playRound(playerSelection, getComputerChoice);
+    }
+    let gameWinner = (max(playerWins, computerWins));
+    console.log(`${gameWinner} won the best of 5!`);
+    return;
+}
+
+
+
+//const playerSelection = prompt("paper scissors or rock?");
+const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let button_print_test = document.createElement("button");
+button_print_test.addEventListener("click", getComputerChoice);
+document.body.appendChild(button_print_test);
+//game();
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
