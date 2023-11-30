7583753a23bc4bf87a5d61d2ae126e6f4582a8e7
plantleafboy <alexhpcp@gmail.com>
2023-12-01 03:29:39 +1300

    finish base animation for status

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let playerWins = 0;
 let computerWins = 0;
+let p_display = document.querySelector(".top-left");
+let c_display = document.querySelector(".top-right");
+let standings = document.querySelector(".status-effect");
+let status_text = document.querySelector(".status-text");
 let p_text = document.querySelector("p");
 let resetDiv = document.querySelector("#resetDiv")
 let resetButton = document.querySelector("#reset");
@@ -21,9 +25,11 @@ let scissors = document.querySelector(".scissors");
 
 let selection = document.querySelector(".selection");
 selection.addEventListener("click", (e) => {
-    console.log(playRound(e.target.value, getComputerChoice())); 
+    (playRound(e.target.value, getComputerChoice())); 
     checkGame();
 })
+
+// let triggerStatus = new CustomEvent('triggerStatus', )
 
 // document.choices.append(button_print_test);
 // button_print_test.addEventListener("click", game);
@@ -37,43 +43,57 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     console.log(`${playerSelection} + ${computerSelection}`);
-
     if (playerSelection === computerSelection) { //logic for picking winner
-        return `Tie! you both picked ${playerSelection}!`; //if tie
+        status_text.textContent = `Tie! you both picked ${playerSelection}!`; //if tie
+        standings.classList.remove('fadeIn');
+        // standings.classList.add('fadeIn');
+        setTimeout(function() {
+            standings.classList.add('fadeIn');
+        }, 10);
     }
     else if (outcomes[playerSelection] === computerSelection) {
-
         playerWins++;
-        return `You Win! ${playerSelection} beats ${computerSelection}!`; //if player win
+        p_display.textContent = `Player: ${playerWins}`;
+        status_text.textContent = `You Win! ${playerSelection} beats ${computerSelection}!`; //if player win
+        
+        standings.classList.remove('fadeIn');
+        // standings.classList.add('fadeIn');
+        setTimeout(function() {
+            standings.classList.add('fadeIn');
+        }, 10);
     }
     else {
         computerWins++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`; //if player lose
+        c_display.textContent = `CP: ${computerWins}`;
+
+        standings.classList.remove('fadeIn');
+        status_text.textContent = `You Lose! ${computerSelection} beats ${playerSelection}!`; //if player lose
+        // standings.classList.add("fadeIn");
+        setTimeout(function() {
+            standings.classList.add('fadeIn');
+        }, 10);
     }
 }
 
 function resetGame() {
     playerWins = 0;
     computerWins = 0;
-
+    p_display.textContent = `Player: ${playerWins}`;
+    c_display.textContent = `CP: ${computerWins}`;
     resetDiv.style.display = 'none';
-    p_text.textContent = (`champions are made here!`)
+    p_text.textContent = (`hello friends!`)
+    status_text.textContent = "Another round...";
 }
 
 function checkGame() {
     if (playerWins === 3 || computerWins === 3) {
         let gameWinner = playerWins > computerWins ? "Player" : "Computer";
-        p_text.textContent = (`${gameWinner} won the best of 5!`)
+        p_text.textContent = `${gameWinner} wins!`;
 
         resetDiv.style.display = 'block';
         return;
-        //alert(`${gameWinner} won the best of 5!`);
     }
-    // else {
-    //     let computerSelection = getComputerChoice();
-    //     console.log(playRound(getPlayerChoice(), computerSelection));
-
-    // }    
+ 
 
     console.log(`pwins: ${playerWins} & cWins: ${computerWins}`)
     console.log("----------------");
